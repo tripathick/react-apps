c2f8df96875e113a20ac9f24150c3d5fc6155c7c
tripathick <chandankumar765414@gmail.com>
2022-03-13 22:02:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Restaur from "./component/basics/Restaur";
+// import UseState from "./component/Hooks/UseState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+import Todo from "./component/todo/todo";
+//import Temp from "./component/weather/temp";
+const App = () => {
+  //return <Restaur />
+  //return <UseState />;
+  //return <UseEffect />
+   //return <UseReducer />
+  return <Todo />
+  //return <Temp />
+  
+};
+export default App;
